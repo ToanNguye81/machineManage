b50f05a109bf5fe509696ad44abf83c78c625a5a
ToanNguy81 <n.t.quoctoan@gmail.com>
2023-12-09 11:55:54 +0700

    add Iso FE

--- src/main/java/com/api/pizza/entity/Ycsc.java
+++ src/main/java/com/api/pizza/entity/Ycsc.java
@@ -1,12 +1,6 @@
 package com.api.pizza.entity;
 
-import java.util.Date;
-import java.util.List;
-
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,9 +12,34 @@ public class Ycsc extends BaseEntity {
 
     @Column(name = "title")
     private String title;
-   
-    @Column(name = "number")
-    private String number;
-    
 
+    @Column(name ="month")
+    private String month;
+    
+    @Column(name ="year")
+    private String year;
+   
+    @Column(name ="index")
+    private Integer index;
+
+    @Column(name = "department")
+    private String department;
+
+    // @Column (department)
+    
+    /* Bấm nút tạo
+        Bước 1: Dò phiếu ycsc để lấy chuỗi cố định theo khu vực
+        Bước 2: Filter theo tháng để lấy theo tháng và năm
+        Bước 3: Tạo phiếu mới đúng số tháng, chỉ mục là số lớn nhất trong danh sách theo tháng và khu vực + 1
+        Bước 4: Load lại thông tin ra màn hình theo khu vực
+    */
+
+
+ /* Điều kiện cần của ycsc database
+  1. title 
+  2. liên kết department
+  3. month/year
+  4. index
+
+  */
 }
